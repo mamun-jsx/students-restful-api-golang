77ded5f06b1bd82101b9f633a75b89af33b6cfc9
almamun528 <ayapths@gmail.com>
2026-09-12 15:36:16 +0600

    gressfully shutdown the server

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/mamun-jsx/students-restful-api-golang/internal/config"
 )
@@ -28,9 +34,24 @@ func main() {
 	}
 	fmt.Printf("Starting server on %s\n", cfg.HttpServer.Address)
 
-	err := server.ListenAndServe()
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		err := server.ListenAndServe()
+
+		if err != nil {
+			log.Fatal("failed to start server")
+		}
+	}()
+	<-done
+	slog.Info("shutting down the server")
+	ctx, cancle := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancle()
+
+	err := server.Shutdown(ctx)
 	if err != nil {
-		log.Fatal("failed to start server")
+		slog.Error("Failed to shutdown server ", slog.String("error", err.Error()))
 	}
+	slog.Info("server gracefully stopped")
 
 }
